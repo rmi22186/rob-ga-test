6a6aacbe81465937deb65eb5895704ba72480ea5
mParticle Automation <developers@mparticle.com>
2021-01-06 18:35:39 +0000

    Generate latest bundle

---
+++ dist/test-ga.common.js
@@ -0,0 +1,13 @@
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function register() {
+  console.log("registered");
+}
+
+var testingGa = {
+  register: register,
+};
+var testingGa_1 = testingGa.register;
+
+exports.default = testingGa;
+exports.register = testingGa_1;
---
+++ dist/test-ga.iife.js
@@ -0,0 +1,16 @@
+var gatesting = (function (exports) {
+  function register() {
+    console.log("registered");
+  }
+
+  var testingGa = {
+    register: register,
+  };
+  var testingGa_1 = testingGa.register;
+
+  exports.default = testingGa;
+  exports.register = testingGa_1;
+
+  return exports;
+
+}({}));
