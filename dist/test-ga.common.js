9c3ed6e38c5f8d0f24a1ac441edb14920d7c8173
mParticle Automation <developers@mparticle.com>
2021-03-08 16:05:35 +0000

    Generate latest bundle

--- dist/test-ga.common.js
+++ dist/test-ga.common.js
@@ -1,5 +1,26 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
+/*!
+ * isobject <https://github.com/jonschlinkert/isobject>
+ *
+ * Copyright (c) 2014-2017, Jon Schlinkert.
+ * Released under the MIT License.
+ */
+
+function isObject(val) {
+  return val != null && typeof val === 'object' && Array.isArray(val) === false;
+}
+
+var isobject = /*#__PURE__*/Object.freeze({
+  'default': isObject
+});
+
+function getCjsExportFromNamespace (n) {
+	return n && n['default'] || n;
+}
+
+getCjsExportFromNamespace(isobject);
+
 function register() {
   console.log("registered");
 }
